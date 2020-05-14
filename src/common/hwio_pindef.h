2bb39f7ab60bc8ce6ae2a44a2992d5e8da6319a0
Marek Bel <marek.bel@posta.cz>
2020-05-14 21:43:58 +0200

    Remove PA0 .. PE15 defines to find out where it is defined and where it is used.

--- src/common/hwio_pindef.h
+++ src/common/hwio_pindef.h
@@ -17,22 +17,22 @@
 
 #define PIN_X_DIR    PD0
 #define PIN_X_STEP   PD1
-#define PIN_X_ENABLE PD3
+#define PIN_X_ENABLE TPD3
 #define PIN_X_DIAG   PE2
 
 #define PIN_Y_DIR    PD12
 #define PIN_Y_STEP   PD13
-#define PIN_Y_ENABLE PD14
+#define PIN_Y_ENABLE TPD14
 #define PIN_Y_DIAG   PE1
 
 #define PIN_Z_DIR    PD15
 #define PIN_Z_STEP   PD4
-#define PIN_Z_ENABLE PD2
+#define PIN_Z_ENABLE TPD2
 #define PIN_Z_DIAG   PE3
 
 #define PIN_E_DIR    PD8
 #define PIN_E_STEP   PD9
-#define PIN_E_ENABLE PD10
+#define PIN_E_ENABLE TPD10
 #define PIN_E_DIAG   PA15
 
 #define PIN_FAN1    PE9
@@ -41,6 +41,6 @@
 #define PIN_BTN_EN1 PE13
 #define PIN_BTN_EN2 PE15
 
-#define PIN_FSENSOR PB4
+#define PIN_FSENSOR TPB4
 
 #endif //_HWIO_PINDEF_H
