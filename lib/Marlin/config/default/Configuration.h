MarlinFirmware/Marlin

Make small segment handling in JD configurable
The curvature-based handling of small segments creates problems, in case the slicer does not perfectly distribute all junction angles. This change makes the handling user-selectable.

Marlin/Configuration.h
@@ -815,6 +815,7 @@
  */
 #if DISABLED(CLASSIC_JERK)
   #define JUNCTION_DEVIATION_MM 0.013 // (mm) Distance from real junction edge
+  #define JUNCTION_DEVIATION_SMALL_SEGMENT_HANDLING // Handle small segments (< 1 mm) with large junction angles (> 135°) based on a local curvature estimate, instead of just the junction angle.
 #endif
 
 /**

mkbel/Prusa-Firmware-Buddy

lib/Marlin/config/default/Configuration.h
@@ -786,6 +786,7 @@
  */
 #if DISABLED(CLASSIC_JERK)
   #define JUNCTION_DEVIATION_MM 0.013 // (mm) Distance from real junction edge
+  #define JD_SMALL_SEGMENT_HANDLING // Handle small segments (< 1 mm) with large junction angles (> 135°) based on a local curvature estimate, instead of just the junction angle.
 #endif
 
 /**